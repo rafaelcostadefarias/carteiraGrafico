6a7e47ae2a7cacc88a0812822b58ad4e8c10759f
rafaelcostadefarias <rafaelcostadefarias@gmail.com>
2018-04-02 16:39:49 -0300

    Teste do POD Charts

--- carteiraGrafico/ViewController.swift
+++ carteiraGrafico/ViewController.swift
@@ -7,19 +7,76 @@
 //
 
 import UIKit
+import Charts
 
 class ViewController: UIViewController {
 
+    //MARK: Outlets
+    @IBOutlet weak var rfStepper: UIStepper!
+    @IBOutlet weak var rvStepper: UIStepper!
+    
+    @IBOutlet weak var pieCarteira: PieChartView!
+    
+    //MARK: Propriedades
+    var rfDataEntry = PieChartDataEntry(value: 0)
+    var rvDataEntry = PieChartDataEntry(value: 0)
+    
+    var alocacaoCarteira = [PieChartDataEntry]()
+    
+    
+    //MARK: Métodos ViewContriller
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        rfStepper.value = 30
+        rvStepper.value = 70
+        
+        pieCarteira.chartDescription?.text = ""
+        
+        rfDataEntry.value = rfStepper.value
+        rfDataEntry.label = "Renda Fixa"
+        
+        rvDataEntry.value = rvStepper.value
+        rvDataEntry.label = "Renda Variável"
+        
+        alocacaoCarteira = [rfDataEntry, rvDataEntry]
+        
+        updateChartData()
+        
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    //MARK: Actions
+
+    @IBAction func changeRF(_ sender: UIStepper) {
+        rfDataEntry.value = sender.value
+        rvDataEntry.value = abs(sender.value - 100)
+        rvStepper.value = rvDataEntry.value
+        updateChartData()
+        
     }
+    
 
-
+    @IBAction func changeRV(_ sender: UIStepper) {
+        rvDataEntry.value = sender.value
+        rfDataEntry.value = abs(sender.value - 100)
+        rfStepper.value = rfDataEntry.value
+        updateChartData()
+    }
+    
+    //MARK: Métodos próprios
+    func updateChartData(){
+        
+        let chartDataSet = PieChartDataSet(values: alocacaoCarteira, label: nil)
+        let chartData = PieChartData(dataSet: chartDataSet)
+        
+        let pieColors = [UIColor.blue, UIColor.red]
+        
+        chartDataSet.colors = pieColors
+        
+        pieCarteira.data = chartData
+        
+    }
+    
 }
 
